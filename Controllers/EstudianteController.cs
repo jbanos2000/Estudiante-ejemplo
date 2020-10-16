965d210426053b2a73f4798d03dee89486e4a32a
Jesus <jesusbacha2000@gmail.com>
2020-10-16 15:31:15 -0500

    Estudiante Controller

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvc_lab.Models;
+
+namespace mvc_lab.Controllers{
+    public class EstudianteController : Controller{
+
+        private readonly ILogger<EstudianteController> _logger;
+
+        public EstudianteController(ILogger<EstudianteController> logger){
+
+            _logger=logger;
+        }
+
+        public IActionResult Index(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Almacenar(Estudiante estudiante){
+            if(ModelState.IsValid){
+                estudiante.Answer="Registro de Base de Datos (*)";
+            }
+
+            return View("Index",estudiante);
+        }
+    }
+}
